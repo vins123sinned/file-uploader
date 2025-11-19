94d4343c030fbb040c865712d04316d31ae0e86e
Vinson <vinsonhe2@gmail.com>
2025-11-19 09:02:04 -0800

    chore: set up EJS templating language

--- app.js
+++ app.js
@@ -5,6 +5,9 @@ const app = express();
 
 app.get("/", (req, res) => res.send("Who goes there?"));
 
+app.set("views", path.join(import.meta.dirname, "views"));
+app.set("view engine", "ejs");
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT || 3000, (err) => {
   if (err) throw err;
