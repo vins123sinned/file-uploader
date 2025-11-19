4098a91a25e1e9116bb51777d04cd2b0c65e68cf
Vinson <vinsonhe2@gmail.com>
2025-11-19 12:00:56 -0800

    chore: set up Prisma ORM
    Install all packages and create an initial schema
    
    - Add comments in `app.js` for clarification

--- app.js
+++ app.js
@@ -5,9 +5,11 @@ const app = express();
 
 app.get("/", (req, res) => res.send("Who goes there?"));
 
+// configure views directory and set EJS as the view engine
 app.set("views", path.join(import.meta.dirname, "views"));
 app.set("view engine", "ejs");
 
+// configure the path of static assets (css, images, etc)
 const assetsPath = path.join(import.meta.dirname, "public");
 app.use(express.static(assetsPath));
 
---
+++ prisma/migrations/20251119195959_init/migration.sql
@@ -0,0 +1,34 @@
+-- CreateTable
+CREATE TABLE "Session" (
+    "id" TEXT NOT NULL,
+    "sid" TEXT NOT NULL,
+    "data" TEXT NOT NULL,
+    "expiresAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Session_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "File" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+    "date" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "size" INTEGER NOT NULL,
+    "folderId" INTEGER NOT NULL,
+
+    CONSTRAINT "File_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Folder" (
+    "id" SERIAL NOT NULL,
+    "name" TEXT NOT NULL,
+
+    CONSTRAINT "Folder_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Session_sid_key" ON "Session"("sid");
+
+-- AddForeignKey
+ALTER TABLE "File" ADD CONSTRAINT "File_folderId_fkey" FOREIGN KEY ("folderId") REFERENCES "Folder"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
