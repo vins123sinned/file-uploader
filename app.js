3ee80d6bb63b2411048a62807b28f79a34024f38
Vinson <vinsonhe2@gmail.com>
2025-11-19 09:37:28 -0800

    chore: set up static assets directory

--- app.js
+++ app.js
@@ -8,6 +8,9 @@ app.get("/", (req, res) => res.send("Who goes there?"));
 app.set("views", path.join(import.meta.dirname, "views"));
 app.set("view engine", "ejs");
 
+const assetsPath = path.join(import.meta.dirname, "public");
+app.use(express.static(assetsPath));
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT || 3000, (err) => {
   if (err) throw err;
