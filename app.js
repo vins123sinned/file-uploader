761807ffd415441ad93e435ddb1341e9f4200891
Vinson <vinsonhe2@gmail.com>
2025-11-20 13:08:51 -0800

    feat(authentication): create login route and view
    Also create a simple homepage to link to the login page
    
    - Install Passport.js' required packages
    - Create `authenticationRouter.js` and `authenticationController.js`
      file to place authentication code
    - Create `login.ejs` view
    - Connect router in `app.js`
    - Add a `User` model in `schema.prisma` for authentication

--- app.js
+++ app.js
@@ -1,9 +1,13 @@
 import "dotenv/config";
+import path from "node:path";
 import express from "express";
+import session from "express-session";
+import passport from "passport";
+import { authenticationRouter } from "./routes/authenticationRouter.js";
 
 const app = express();
 
-app.get("/", (req, res) => res.send("Who goes there?"));
+app.get("/", (req, res) => res.render("basicHomepage"));
 
 // configure views directory and set EJS as the view engine
 app.set("views", path.join(import.meta.dirname, "views"));
@@ -12,6 +16,22 @@ app.set("view engine", "ejs");
 // configure the path of static assets (css, images, etc)
 const assetsPath = path.join(import.meta.dirname, "public");
 app.use(express.static(assetsPath));
+
+// session secret for Passport.js cookies
+// Make sure to add validation!
+/*
+app.use(session({
+  cookie: {
+
+  }
+}));
+app.use(passport.session());
+*/
+
+// Parse the urlencoded data sent by the form's POST
+app.use(express.urlencoded({ extended: false }));
+
+app.use("/", authenticationRouter);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT || 3000, (err) => {
