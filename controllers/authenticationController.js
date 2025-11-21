a4023a40a706716f177f40b583bcb74e684f3c42
Vinson <vinsonhe2@gmail.com>
2025-11-21 14:29:52 -0800

    feat(authentication): polish signup validation
    - Add a `notEmpty()` validator to the `email` field
    - Add a custom validator to prevent any spaces in the `password` field
    - Update `password` field's RegEx to be simpler and more efficient
    - Accept any non-alphanumeric character in addition to the regular
      special characters for passwords
    - Add `return true` line at the end of custom validators to indicate the
      field is valid
    - Add forgotten `bail()` functions

--- controllers/authenticationController.js
+++ controllers/authenticationController.js
@@ -5,36 +5,58 @@ const lengthErr = (minLength, maxLength) =>
   `must be between ${minLength} and ${maxLength} characters`;
 const emailErr = "must be a valid email address";
 const inUseErr = "is already registered";
+const spaceError = "must not include any spaces";
 const passwordError =
-  "must have at least 8 characters, one number, and one special character (~`! @#$%^&*()_-+={[}]|\:;\"'<,>.?/)";
+  "must have at least 8 characters, one number, and one special character";
 
 const validateSignUp = [
   body("email")
     .trim()
+    .notEmpty()
+    .withMessage(`Email ${requiredErr}`)
+    .bail()
     .isLength({ min: 3, max: 254 })
     .withMessage(`Email ${lengthErr(3, 254)}`)
+    .bail()
     .isEmail()
     .withMessage(`Email ${emailErr}`)
+    .bail()
     .custom(async (value) => {
       // const email = await (CHECK IF EMAIL EXISTS IN DATABASE ALREADY)
+      const email = false;
 
       if (email) {
         throw new Error("Email already in database");
       }
+
+      return true;
     })
     .withMessage(`Email ${inUseErr}`),
   body("password")
-    .trim()
+    // omit trim here to let users know NO SPACES (even at the start or end)
     .notEmpty()
     .withMessage(`Password ${requiredErr}`)
+    .bail()
     .custom((value) => {
-      // at least 8 characters, one number, and one special character (~`! @#$%^&*()_-+={\[}\]|\:;"'<,>.?/)
-      const regex =
-        /^(?=.*[A-Za-z])(?=.*\d)(?=.*[~`! @#$%^&*()_\-+={\[}\]|\:;"'<,>.?\/])[A-Za-z\d~`! @#$%^&*()_\-+={\[}\]|\:;"'<,>.?\/]{8,}$/gm;
+      const spaceRegex = /\s/;
 
-      if (!value.test(regex)) {
+      if (spaceRegex.test(value)) {
+        throw new Error("Password contains spaces");
+      }
+
+      return true;
+    })
+    .withMessage(`Password ${spaceError}`)
+    .bail()
+    .custom((value) => {
+      // at least 8 characters, one number, and one non-alphanumeric (NO SPACES!)
+      const regex = /^(?=.*\d)(?=.*[^\w\s])[^\s]{8,}$/;
+
+      if (!regex.test(value)) {
         throw new Error("Password does not pass the constraints");
       }
+
+      return true;
     })
     .withMessage(`Password ${passwordError}`),
 ];
@@ -53,6 +75,7 @@ const postSignUp = [
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(400).render("signup", {
+        previousValues: req.body,
         errors: errors.array(),
       });
     }
