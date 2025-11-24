91996931b56063b318c66b81e0f910c5e5af189e
Vinson <vinsonhe2@gmail.com>
2025-11-24 09:35:45 -0800

    chore: move queries into User class
    For better readability and maintainability, move all queries in
    `queries.js` to `User.js`

---
+++ db/User.js
@@ -0,0 +1,43 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
+
+class User {
+  async getUserById(id) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id: id,
+      },
+    });
+
+    return user;
+  }
+
+  async getUserByEmail(email) {
+    // use findFirst instead of findUnique for case-insensitive search
+    const user = await prisma.user.findFirst({
+      where: {
+        email: {
+          equals: email,
+          mode: "insensitive",
+        },
+      },
+    });
+
+    return user;
+  }
+
+  async insertUser(email, password) {
+    // add bcrypt soon!
+    await prisma.user.create({
+      data: {
+        email: email,
+        password: password,
+      },
+    });
+
+    return;
+  }
+}
+
+export const userDb = new User();
