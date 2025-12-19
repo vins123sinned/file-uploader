058d28118c67c117e462d7e6235aba21e32fda8e
Vinson 2 <vinsonhe2@gmail.com>
2025-12-18 20:27:55 -0800

    feat(upload): add client side files validation
    In the client side, accept only PNGs and JPG/JPEGs as well as limiting
    the files to 1 MB or lower

--- public/js/uploadForm.js
+++ public/js/uploadForm.js
@@ -1,7 +1,28 @@
 const fileInput = document.querySelector("#images");
+const fileInputError = document.querySelector(".file-input-error");
 const fileCountSpan = document.querySelector(".file-count");
 
-const updateFileCount = () => {
+function fileChange() {
+  checkFileSize();
+  updateFileCount();
+}
+
+function checkFileSize() {
+  for (const file of Array.from(fileInput.files)) {
+    // 1000000 bytes is a megabyte!
+    if (file.size >= 1000000) {
+      fileInput.value = "";
+      fileInputError.classList.add("invalid");
+      fileInputError.textContent = "Files must not exceed 1 MB";
+      return;
+    }
+  }
+
+  fileInputError.classList.remove("invalid");
+  fileInputError.textContent = "";
+}
+
+function updateFileCount() {
   const fileCount = fileInput.files.length;
 
   if (fileCount > 8) {
@@ -10,9 +31,9 @@ const updateFileCount = () => {
     fileCountSpan.classList.remove("invalid");
   }
   fileCountSpan.textContent = fileCount;
-};
+}
 
-fileInput.addEventListener("change", updateFileCount);
+fileInput.addEventListener("change", fileChange);
 
 // initial file count for page refreshes
 updateFileCount();
