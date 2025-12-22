dda5ef03a0d1cf4ace38e43dfca010a3c3df148e
Vinson 2 <vinsonhe2@gmail.com>
2025-12-21 22:25:28 -0800

    feat(upload): add delete for image previews
    On the client side, allow image previews to be deleted since the file
    input is no longer the source of truth for the images (now it's the
    `files` array)

--- public/js/uploadForm.js
+++ public/js/uploadForm.js
@@ -3,24 +3,41 @@ const fileInputError = document.querySelector(".file-input-error");
 const fileCountSpan = document.querySelector(".file-count");
 const imagesPreviews = document.querySelector(".images-previews");
 
-const files = [];
+let files = [];
 
 function fileChange() {
   checkFileSize();
   showImagesPreviews();
   updateFileCount();
-  console.log(files);
 }
 
 function showImagesPreviews() {
   Array.from(fileInput.files).forEach((file) => {
+    const previewContainer = document.createElement("div");
     const image = document.createElement("img");
     image.alt = `Preview image of ${file.name}`;
     image.height = "200";
     image.src = URL.createObjectURL(file);
+    const deleteButton = document.createElement("button");
+    deleteButton.type = "button";
+    deleteButton.setAttribute("aria-label", "Delete image");
+    const deleteIcon = document.createElement("span");
+    deleteIcon.classList.add("material-symbols-outlined");
+    deleteIcon.textContent = "delete";
 
-    imagesPreviews.appendChild(image);
+    deleteButton.appendChild(deleteIcon);
+    previewContainer.appendChild(image);
+    previewContainer.appendChild(deleteButton);
+
+    imagesPreviews.appendChild(previewContainer);
     files.push(file);
+
+    deleteButton.addEventListener("click", () => {
+      imagesPreviews.removeChild(previewContainer);
+      files = files.filter((item) => item !== file);
+
+      updateFileCount();
+    });
   });
 }
 
@@ -44,7 +61,7 @@ function checkFileSize() {
 }
 
 function updateFileCount() {
-  const fileCount = fileInput.files.length;
+  const fileCount = files.length;
 
   if (fileCount > 8) {
     fileCountSpan.classList.add("invalid");
