bb95dda68c8317dcbe1690dbb23f2ccff20df13f
Vinson 2 <vinsonhe2@gmail.com>
2025-12-30 23:32:32 -0800

    feat(folders): add share button functionality
    Also added extra logic check for share/delete form to see if the action
    link is the same when their respective buttons are clicked. If it is,
    then hide the form, otherwise switch the form out without hiding for a
    better UX.
    
    NOTE: This is for the client side!

--- public/js/list.js
+++ public/js/list.js
@@ -1,16 +1,38 @@
+const shareButtons = document.querySelectorAll(".share-button");
+const shareForm = document.querySelector(".share-form");
 const deleteButtons = document.querySelectorAll(".delete-button");
 const deleteForm = document.querySelector(".delete-form");
 
 deleteButtons.forEach((deleteButton) => {
   deleteButton.addEventListener("click", () => {
-    if (deleteForm.classList.contains("hidden")) {
-      const actionLink = `/${deleteButton.dataset.subject}/delete/${deleteButton.dataset.id}`;
+    const actionLink = `/${deleteButton.dataset.subject}/delete/${deleteButton.dataset.id}`;
 
+    if (
+      deleteForm.getAttribute("action") !== actionLink ||
+      deleteForm.classList.contains("hidden")
+    ) {
       deleteForm.setAttribute("action", actionLink);
       deleteForm.classList.remove("hidden");
     } else {
       deleteForm.setAttribute("action", "");
       deleteForm.classList.add("hidden");
+    }
+  });
+});
+
+shareButtons.forEach((shareButton) => {
+  const actionLink = `/${shareButton.dataset.subject}/share/${shareButton.dataset.id}`;
+
+  shareButton.addEventListener("click", () => {
+    if (
+      shareForm.getAttribute("action") !== actionLink ||
+      shareForm.classList.contains("hidden")
+    ) {
+      shareForm.setAttribute("action", actionLink);
+      shareForm.classList.remove("hidden");
+    } else {
+      shareForm.setAttribute("action", "");
+      shareForm.classList.add("hidden");
     }
   });
 });
