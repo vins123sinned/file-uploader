5e7a4f6385066c2f84abf9e043c0bcaf5598685f
Vinson 2 <vinsonhe2@gmail.com>
2026-01-12 15:42:38 -0800

    feat(homepage): add navbar JS functionality
    On mobile screens, use can click on icons to open their respective
    dropdowns, making the navbar much easier and nicer on the eyes

---
+++ public/js/navbar.js
@@ -0,0 +1,35 @@
+const menuIcon = document.querySelector(".menu-icon");
+const profilePicture = document.querySelector(".profile-picture");
+const postFolderLinks = document.querySelector(".post-folder-links");
+const userLinks = document.querySelector(".user-links");
+
+window.addEventListener("click", (event) => {
+  if (
+    !event.target.classList.contains("menu-icon") &&
+    !event.target.classList.contains("post-folder-links")
+  ) {
+    postFolderLinks.classList.add("hidden");
+  }
+  if (
+    !event.target.classList.contains("profile-picture") &&
+    !event.target.classList.contains("user-links")
+  ) {
+    userLinks.classList.add("hidden");
+  }
+});
+
+menuIcon.addEventListener("click", () => {
+  if (postFolderLinks.classList.contains("hidden")) {
+    postFolderLinks.classList.remove("hidden");
+  } else {
+    postFolderLinks.classList.add("hidden");
+  }
+});
+
+profilePicture.addEventListener("click", () => {
+  if (userLinks.classList.contains("hidden")) {
+    userLinks.classList.remove("hidden");
+  } else {
+    userLinks.classList.add("hidden");
+  }
+});
