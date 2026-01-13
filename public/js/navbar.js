3921a4e7940b5719b97b491ec8255b6ba2d280d1
Vinson 2 <vinsonhe2@gmail.com>
2026-01-13 11:28:21 -0800

    feat(homepage): add food emojis to navbar
    On desktop screens (1000px+), add random food emojis that wiggles on
    hover

--- public/js/navbar.js
+++ public/js/navbar.js
@@ -2,6 +2,7 @@ const menuIcon = document.querySelector(".menu-icon");
 const profilePicture = document.querySelector(".profile-picture");
 const postFolderLinks = document.querySelector(".post-folder-links");
 const userLinks = document.querySelector(".user-links");
+const foodEmojisList = document.querySelector(".food-emojis-li");
 
 window.addEventListener("click", (event) => {
   if (
@@ -33,3 +34,28 @@ profilePicture.addEventListener("click", () => {
     userLinks.classList.add("hidden");
   }
 });
+
+async function getRandomFoodEmojis() {
+  const url = "https://api.emojisworld.fr/v1/random?categories=5&limit=5";
+  try {
+    const response = await fetch(url);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const result = await response.json();
+    const emojis = result.results;
+
+    for (const emoji of emojis) {
+      const span = document.createElement("span");
+      span.classList.add("emoji");
+      span.textContent = emoji.emoji;
+
+      foodEmojisList.appendChild(span);
+    }
+  } catch (err) {
+    console.error(err.message);
+  }
+}
+
+getRandomFoodEmojis();
